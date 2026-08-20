90b9546c8f9d4563619667022b394538f2e6d29c
Igor Bondarenko <bondarenko200021@mail.ru>
2026-08-20 11:52:16 +0700

    db_helper and settings

---
+++ app/db/db_helper.py
@@ -0,0 +1,31 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from app.core.config import settings
+
+
+class DataBaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool,
+    ):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def close_session(self):
+        if self.engine is None:
+            return await self.engine.dispose()
+
+    async def get_session(self):
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DataBaseHelper(url=settings.db_url, echo=settings.db_echo)
