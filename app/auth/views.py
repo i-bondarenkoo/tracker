3842852b48056e8639a34358941258b622b73619
Igor Bondarenko <bondarenko200021@mail.ru>
2026-09-02 15:42:08 +0700

    /auth/register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, HTTPException, status, Body
+from app.schemas.user import ResponseUser, CreateUser
 from app.models.user import User
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.db_helper import db_helper
+from app.crud import user
+from sqlalchemy.exc import IntegrityError
 from app.schemas.token import ResponseToken
 from app.auth.dependencies import user_auth, create_access_token
 
@@ -8,6 +13,36 @@ router = APIRouter(
     prefix="/auth",
     tags=["Auth"],
 )
+
+
+@router.post(
+    "/register",
+    response_model=ResponseUser,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_user(
+    user_data: Annotated[
+        CreateUser, Body(description="Данные пользователя для создания")
+    ],
+    session: AsyncSession = Depends(db_helper.get_session),
+):
+    check_user = await user.get_user_by_email_crud(
+        email=user_data.email, session=session
+    )
+    if check_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Пользователь с такой почтой уже существует",
+        )
+    try:
+        new_user = await user.create_user_crud(user_data=user_data, session=session)
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Пользователь с такой почтой уже существует",
+        )
+    return new_user
 
 
 @router.post("/login", response_model=ResponseToken)
--- app/views/user.py
+++ app/views/user.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
-from app.models.user import User
+
 from app.db.db_helper import db_helper
 from fastapi import Depends, Body, APIRouter, HTTPException, Query, status, Path
 from app.schemas.user import (
-    CreateUser,
     ResponseUserExtended,
     ResponseUser,
     UpdateUserPatch,
@@ -24,36 +23,6 @@ router = APIRouter(
     tags=["Users"],
     # dependencies=[Depends(get_current_user)],
 )
-
-
-@router.post(
-    "/register",
-    response_model=ResponseUser,
-    status_code=status.HTTP_201_CREATED,
-)
-async def create_user(
-    user_data: Annotated[
-        CreateUser, Body(description="Данные пользователя для создания")
-    ],
-    session: AsyncSession = Depends(db_helper.get_session),
-):
-    check_user = await user.get_user_by_email_crud(
-        email=user_data.email, session=session
-    )
-    if check_user:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Пользователь с такой почтой уже существует",
-        )
-    try:
-        new_user = await user.create_user_crud(user_data=user_data, session=session)
-    except IntegrityError:
-        await session.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Пользователь с такой почтой уже существует",
-        )
-    return new_user
 
 
 @router.get("/{user_id}", response_model=ResponseUserExtended)
